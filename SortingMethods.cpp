e6c8e5898cbc6a5877d5e3e69a44a16b5ff88b5a
Caitlin Dooley <ccdooley94@gmail.com>
2019-10-15 12:44:57 -0500

    Update SortingMethods.cpp

--- SortingMethods.cpp
+++ SortingMethods.cpp
@@ -1,9 +1,8 @@
 // This program displays several methods of sorting an array of integers and compares the time each takes
 
-
 #include <iostream>
-#include <cstdlib>
-#include <time.h>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
@@ -73,37 +72,83 @@ float ins_sort(int a[], int size) {
 
 int main()
 {
-    //ARRAY OF 100 RANDOM NUMBERS
+	//ARRAY OF 100 RANDOM NUMBERS
 	int array_100[100];
+
+	for (int i = 0; i < 100; i++) {
+		array_100[i] = rand();
+		cout << array_100[i] << " ";
+	}
+
+	cout << "To sort an array of 100 numbers, it takes:\n";
+	
+	int bub_100 = bub_sort(array_100, 100);
+	cout << bub_100 << " seconds using bubble sort\n";
+	
+	for (int i = 0; i < 100; i++)
+		array_100[i] = rand();
+	
+	int sel_100 = sel_sort(array_100, 100);
+	cout << sel_100 << " seconds using selection sort\n";
 
 	for (int i = 0; i < 100; i++)
 		array_100[i] = rand();
 
-	cout << "To sort an array of 100 numbers, it takes:\n";
-	cout << bub_sort(array_100, 100) << " seconds using bubble sort\n";
-	cout << sel_sort(array_100, 100) << " seconds using selection sort\n";
-	cout << ins_sort(array_100, 100) << " seconds using insertion sort\n\n";
+	int ins_100 = ins_sort(array_100, 100);
+	cout << ins_100 << " seconds using insertion sort\n\n";
 
 	//ARRAY OF 1000 RANDOM NUMBERS
 	int array_1000[1000];
 
 	for (int i = 0; i < 1000; i++)
 		array_1000[i] = rand();
-
+	
 	cout << "To sort an array of 1000 numbers, it takes:\n";
-	cout << bub_sort(array_1000, 1000) << " seconds using bubble sort\n";
-	cout << sel_sort(array_1000, 1000) << " seconds using selection sort\n";
-	cout << ins_sort(array_1000, 1000) << " seconds using insertion sort\n\n";
+
+	int bub_1000 = bub_sort(array_1000, 1000);
+	cout << bub_1000 << " seconds using bubble sort\n";
+
+	for (int i = 0; i < 1000; i++)
+		array_1000[i] = rand();
+	
+	int sel_1000 = sel_sort(array_1000, 1000);
+	cout << sel_1000 << " seconds using selection sort\n";
+
+	for (int i = 0; i < 1000; i++)
+		array_1000[i] = rand();
+
+	int ins_1000 = ins_sort(array_1000, 1000);
+	cout << ins_1000 << " seconds using insertion sort\n\n";
 	
 	//ARRAY OF 20000 RANDOM NUMBERS
 	int array_20000[20000];
 
 	for (int i = 0; i < 20000; i++)
 		array_20000[i] = rand();
+	
+	cout << "To sort an array of 20000 numbers, it takes:\n";
 
-	cout << "To sort an array of 2000 numbers, it takes:\n";
-	cout << bub_sort(array_20000, 20000) << " seconds using bubble sort\n";
-	cout << sel_sort(array_20000, 20000) << " seconds using selection sort\n";
-	cout << ins_sort(array_20000, 20000) << " seconds using insertion sort\n\n";
+	int bub_20000 = bub_sort(array_20000, 20000);
+	cout << bub_20000 << " seconds using bubble sort\n";
+	
+	for (int i = 0; i < 20000; i++)
+		array_20000[i] = rand();
+
+	int sel_20000 = sel_sort(array_20000, 20000);
+	cout << sel_20000 << " seconds using selection sort\n";
+
+	for (int i = 0; i < 20000; i++)
+		array_20000[i] = rand();
+
+	int ins_20000 = ins_sort(array_20000, 20000);
+	cout << ins_20000 << " seconds using insertion sort\n\n";
+	
+	
+	cout << "\tbubble(sec) \tselection(sec) \tinsertion(sec)\n";
+	cout << "100\t" << bub_100 << "\t\t" << sel_100 << "\t\t" << ins_100 << endl;
+	cout << "1000\t" << bub_1000 << "\t\t" << sel_1000 << "\t\t" << ins_1000 << endl;
+	cout << "20000\t" << bub_20000 << "\t\t" << sel_20000 << "\t\t" << ins_20000 << endl;
+	
+
+	return 0;
 }
-
